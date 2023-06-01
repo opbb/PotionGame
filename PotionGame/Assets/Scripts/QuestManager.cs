e3b255b05f8faba100e6f33f39cf7353c2f3709a
chloeylee <lee.chloe1@northeastern.edu>
2023-05-31 21:56:26 -0500

    quest manager stuff

--- PotionGame/Assets/Scripts/QuestManager.cs
+++ PotionGame/Assets/Scripts/QuestManager.cs
@@ -8,18 +8,27 @@ public class QuestManager : MonoBehaviour
     public string questInfo;
     public float questTime = 100;
 
+    public Text questText;
+
     bool inQuest = false;
+    Rect textWindow = new Rect(0, 0, 200, 150);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MouseLook.isUIActive = false;
+            Cursor.visible = false;
+
+        }
     }
 
     // first screen when seeing quest
@@ -38,15 +47,23 @@ public class QuestManager : MonoBehaviour
     }
 
     // Accepting quest ui
-    void QuestAccept()
+    public void QuestAccept()
     {
         
     }
 
     // Rejecting quest ui
-    void QuestReject()
+    public void QuestReject()
     {
-
+        if (inQuest == true)
+        {
+            GUI.TextField(textWindow, "Seems that you're too busy for this task at the moment." +
+                "Finish what you're doing and come back when you have the time!");
+        }
+        else
+        {
+            GUI.TextField(textWindow, "You don't want to help me? Ok bye.");
+        }
     }
 
     // Quest succeeded!
