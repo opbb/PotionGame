ba7ac3eb3fc03bb287a54b6119e11019e5ca1175
chloeylee <lee.chloe1@northeastern.edu>
2023-06-08 22:48:00 -0500

    more day and night changes
    **trying** to lerp intensity and color to give darker effect at night

--- PotionGame/Assets/Scripts/DayNightCycle.cs
+++ PotionGame/Assets/Scripts/DayNightCycle.cs
@@ -6,13 +6,61 @@ using System;
 
 public class DayNightCycle : MonoBehaviour
 {
-    public GameObject lightSource;
-    public float timeSpeed = 0.1f;
+    public Light lightSource;
+    public float speed = 0.1f;
     public float time;
+
+    public float dayIntensity = 1;
+    public float nightIntensity = 0;
+    public float intensity;
+
+    public Color dayColor;
+    public Color nightColor;
+    public Color lightColor;
 
     private void Update()
     {
-        time += timeSpeed * Time.deltaTime;
+        Vector3 lightAngle = transform.localEulerAngles;
+
+        time += speed * Time.deltaTime;
         lightSource.transform.rotation = Quaternion.Euler(time, 0, 0);
+
+        intensity = lightSource.intensity;
+
+        if (lightAngle.x >= 0 && lightAngle.x < 90)
+        {
+            intensity = Mathf.Lerp(nightIntensity, dayIntensity, 100 * Time.deltaTime);
+        }
+        else if (lightAngle.x >= 90 && lightAngle.x < 120)
+        {
+            intensity = dayIntensity;
+        }
+        else if (lightAngle.x >= 120 && lightAngle.x < 180)
+        {
+            intensity = Mathf.Lerp(dayIntensity, nightIntensity, 100 * Time.deltaTime);
+        }
+        else if (lightAngle.x >= 180 && lightAngle.x < 0)
+        {
+            intensity = nightIntensity;
+        }
+
+        lightColor = lightSource.color;
+
+        if (lightAngle.x >= 0 && lightAngle.x < 90)
+        {
+            lightColor = Color.Lerp(nightColor, dayColor, speed * Time.deltaTime);
+        }
+        else if (lightAngle.x >= 90 && lightAngle.x < 120)
+        {
+            lightColor = dayColor;
+        }
+        else if (lightAngle.x >= 120 && lightAngle.x < 180)
+        {
+            lightColor = Color.Lerp(dayColor, nightColor, speed * Time.deltaTime);
+        }
+        else if (lightAngle.x >= 180 && lightAngle.x < 0)
+        {
+            lightColor = nightColor;
+        }
     }
 }
